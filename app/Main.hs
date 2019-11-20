6200ad9edbdb81f51e6cd2d301d19a18b8dddbb6
Joshua Brot <jbrot@umich.edu>
2019-11-19 20:30:03 -0500

    We can now parse the colors in the frame.

--- app/Main.hs
+++ app/Main.hs
@@ -11,6 +11,74 @@ import Test.WebDriver
 import Test.WebDriver.Commands.Wait
 import Test.WebDriver.JSON (ignoreReturn)
 
+data Color = None | Garbage 
+                  | I | IShadow
+                  | J | JShadow 
+                  | L | LShadow 
+                  | O | OShadow 
+                  | S | SShadow
+                  | T | TShadow
+                  | Z | ZShadow
+    deriving Eq
+
+colorMap = [ ((  0,  0,  0), None)
+           , ((153,153,153), Garbage)
+           , (( 15,155,215), I)
+           , ((  7, 77,107), IShadow)
+           , (( 33, 65,198), J)
+           , (( 16, 32, 99), JShadow)
+           , ((227, 91,  2), L)
+           , ((113, 45,  1), LShadow)
+           , ((227,159,  2), O)
+           , ((113, 79,  1), OShadow)
+           , (( 89,177,  1), S)
+           , (( 44, 88,  0), SShadow)
+           , ((175, 41,138), T)
+           , (( 87, 20, 69), TShadow)
+           , ((215, 15, 55), Z)
+           , ((107,  7, 27), ZShadow)
+           ]
+charMap = [ (' ', None)
+          , ('X', Garbage)
+          , ('I', I)
+          , ('I', IShadow)
+          , ('J', J)
+          , ('J', JShadow)
+          , ('L', L)
+          , ('L', LShadow)
+          , ('O', O)
+          , ('O', OShadow)
+          , ('S', S)
+          , ('S', SShadow)
+          , ('T', T)
+          , ('T', TShadow)
+          , ('Z', Z)
+          , ('Z', ZShadow)
+          ]
+
+parseColor :: Int -> Int -> Int -> Color
+parseColor r g b = snd . head . filter ((== (r,g,b)) . fst) $ colorMap
+renderColor :: Color -> (Int, Int, Int)
+renderColor c = fst . head . filter ((== c) . snd) $ colorMap
+renderChar :: Color -> Char
+renderChar c = fst . head . filter ((== c) . snd) $ charMap
+
+parseFrame :: [Int] -> [[Color]]
+parseFrame cs = go 0 0 cs
+    where go :: Int -> Int -> [Int] -> [[Color]]
+          go  20  _ _ = []
+          go row 10 colors = []:(go (row + 1) 0 colors)
+          go row col (r:g:b:a:colors) = let (rr:rs) =  go row (col + 1) colors in (parseColor r g b : rr):rs
+          go _ _ _ = [[]]
+
+-- Outputs the pixels extracted to the terminal
+renderFrame :: [[Color]] -> IO ()
+renderFrame = (>> return ()) . sequence . fmap printRow
+  where printChar :: Color -> IO ()
+        printChar c = let (r,g,b) = renderColor c in printf "\x1b[48;2;%d;%d;%dm%c" r g b (renderChar c)
+        printRow :: [Color] -> IO ()
+        printRow = (>> printf "\x1b[0m\n") . sequence . fmap printChar
+
 chromeConfig :: WDConfig
 chromeConfig = useBrowser chrome defaultConfig
 
@@ -25,19 +93,6 @@ waitForGameStart = waitUntil' 10000 600 $ do
     expect $ length capts == 3
     disp <- sequence .  fmap (\e -> cssProp e "display") $ capts
     expect . all (maybe True (== "none")) $ disp
-
--- Outputs the pixels extracted to the terminal
-renderFrame :: [Int] -> IO ()
-renderFrame cs = go 0 0 cs
-    where go :: Int -> Int -> [Int] -> IO ()
-          go  20  _ _ = putStrLn "\x1b[0m"
-          go row 10 colors = do
-                printf "\x1b[0m\n"
-                go (row + 1) 0 colors
-          go row col (r:g:b:a:colors) = do
-                printf "\x1b[48;2;%d;%d;%dm " r g b
-                go row (col + 1) colors
-          go _ _ _ = putStrLn "\x1b[0m"
     
 
 -- Extracts and renders pixels.
@@ -47,7 +102,7 @@ getFrame curr = waitUntil' 10000 10 $ do
     expect $ count > curr
     liftIO $ print count
     display <- executeJS [] "return window.pixels;"
-    liftIO $ renderFrame display
+    liftIO . renderFrame . parseFrame $ display
     return count 
 
 iterateM_ :: Monad m => (a -> m a) -> a -> m a
